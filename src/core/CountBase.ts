a247eef9b3677d6a12db5627767ceb9bb228b51d
nanikaka <nanikaka666@gmail.com>
2025-04-01 01:06:42 +0900

    implemnets CountBase class and applied.

---
+++ src/core/CountBase.ts
@@ -0,0 +1,15 @@
+export abstract class CountBase {
+  readonly value: number;
+  constructor(value: number) {
+    CountBase.#validate(value);
+    this.value = value;
+  }
+  static #validate(value: number) {
+    if (value < 0) {
+      throw new Error("value must be non-negative.");
+    }
+    if (!Number.isSafeInteger(value)) {
+      throw new Error("value is too large for likeCount");
+    }
+  }
+}
--- src/core/LikeCount.ts
+++ src/core/LikeCount.ts
@@ -1,25 +1,17 @@
-export class LikeCount {
+import { CountBase } from "./CountBase";
+
+export class LikeCount extends CountBase {
   readonly videoId: string;
   readonly videoTitle: string;
-  readonly value: number;
   constructor(videoId: string, videoTitle: string, value: number) {
-    LikeCount.#validate(value);
+    super(value);
     this.videoId = videoId;
     this.videoTitle = videoTitle;
-    this.value = value;
   }
   check(nextLikeCount: LikeCount) {
     if (this.videoId !== nextLikeCount.videoId) {
       throw new Error(`Different videoId is detected.`);
     }
     return this.value < nextLikeCount.value;
-  }
-  static #validate(value: number) {
-    if (value < 0) {
-      throw new Error("value must be non-negative.");
-    }
-    if (!Number.isSafeInteger(value)) {
-      throw new Error("value is too large for likeCount");
-    }
   }
 }
