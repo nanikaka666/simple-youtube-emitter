351ba9a9b755098dcca450bcc0897b12d9242c9d
nanikaka <nanikaka666@gmail.com>
2025-04-01 03:21:01 +0900

    fix regex pattern for VideoId

--- src/core/VideoId.ts
+++ src/core/VideoId.ts
@@ -1,7 +1,7 @@
 export class VideoId {
   readonly id: string;
   constructor(id: string) {
-    if (id.match(/[0-9a-zA-Z_-]{11}/) === null) {
+    if (id.match(/^[0-9a-zA-Z_-]{11}$/) === null) {
       throw new Error("invalid format videoId.");
     }
 
