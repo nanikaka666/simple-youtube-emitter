b275984891bed864374dfd8f7f1c671105edb78b
nanikaka <nanikaka666@gmail.com>
2025-04-03 06:55:36 +0900

    add test for SafePollingInterval

---
+++ test/core/SafePollingInterval.test.ts
@@ -0,0 +1,10 @@
+import { SafePollingInterval } from "../../src/core/SafePollingInterval";
+
+test("valid value", () => {
+  const input = 10 * 1000;
+  expect(new SafePollingInterval(input).value).toBe(input);
+});
+
+test("too small value is invalid", () => {
+  expect(() => new SafePollingInterval(10 * 1000 - 1).value).toThrow;
+});
