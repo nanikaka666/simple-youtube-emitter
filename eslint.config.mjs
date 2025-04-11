2cea49471ed219668e2a613f303edae948023bd5
nanikaka <nanikaka666@gmail.com>
2025-04-12 01:17:00 +0900

    add eslint.config.mjs file

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    ignores: ["dist/**", "src/debug.ts", "eslint.config.mjs", "jest.config.js"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
